c746fae7e110de48078dd48ec6ba236003061e13
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2024-04-23 20:44:45 +0530

    Update consumer.py

--- containerized-rabbitmq-listener/consumer.py
+++ containerized-rabbitmq-listener/consumer.py
@@ -1,6 +1,11 @@
 import pika
 import os
 
+import requests
+from requests.auth import HTTPBasicAuth
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+
 def main():
     username = os.getenv("USERNAME")
     vhost = os.getenv("VHOST")
@@ -9,7 +14,20 @@ def main():
     queueName = "TestQueue"
     exchange = 'choreo'
 
+    serviceURL = os.getenv("SVC_URL")
+    consumerKey = os.getenv("CONSUMER_KEY")
+    consumerSecret = os.getenv("CONSUMER_SECRET")
+    tokenURL = os.getenv("TOKEN_URL")
+
+    
+
+
     if (username != None and vhost != None and host != None and password != None):
+        
+        client = BackendApplicationClient(client_id=consumerKey)
+        oauth = OAuth2Session(client=client)
+        _ = oauth.fetch_token(token_url=tokenURL,client_id=consumerKey,client_secret=consumerSecret,auth=HTTPBasicAuth(consumerKey, consumerSecret))
+        response = oauth.get(serviceURL)
 
         credentials = pika.PlainCredentials(username, password)
         connection = pika.BlockingConnection(pika.ConnectionParameters(
@@ -25,6 +43,7 @@ def main():
         def callbackFunctionForQueueA(ch, method, properties, body):
             print('Message is received from Queue {}. Message is : '.format(
                 queueName), body)
+            print("Response from the service: ", response.text)
 
         # attaching consumer callback function to the queue
         channel.basic_consume(
