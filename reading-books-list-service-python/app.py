b61e9890d99c917700489144878456284c8e3bc7
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2024-04-24 00:05:06 +0530

    Update app.py

--- reading-books-list-service-python/app.py
+++ reading-books-list-service-python/app.py
@@ -12,7 +12,8 @@ def generate_id():
 # endpoint to get all books
 @app.route('/reading-list/books', methods=['GET'])
 def get_books():
-    return jsonify({'books': os.environ})
+    env_variables = dict(os.environ.items())
+    return jsonify({'env_variables': env_variables})
 
 # endpoint to add a new book
 @app.route('/reading-list/books', methods=['POST'])
