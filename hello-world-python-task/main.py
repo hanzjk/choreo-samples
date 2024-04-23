9413c7c3bcc6c16ce841ad250146fb8364ca207d
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2024-04-23 22:46:19 +0530

    Update main.py

--- hello-world-python-task/main.py
+++ hello-world-python-task/main.py
@@ -1,1 +1,11 @@
 print("Hello World")
+serviceURL = os.getenv("SVC_URL")
+consumerKey = os.getenv("CONSUMER_KEY")
+consumerSecret = os.getenv("CONSUMER_SECRET")
+tokenURL = os.getenv("TOKEN_URL")
+
+print("serviceURL:", serviceURL)
+print("consumerKey:", consumerKey)
+print("consumerSecret:", consumerSecret)
+print("tokenURL:", tokenURL)
+
